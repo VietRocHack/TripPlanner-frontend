da0f6fbe45fc69834a056c19643a6b10edad728d
Bon Nguyen <anh.xk.nguyen@gmail.com>
2024-06-15 11:54:46 +0700

    edited readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import HomeScreen from './pages/HomeScreen/HomeScreen'
-import PlanScreen from './pages/PlanScreen/PlanScreen'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomeScreen from "./pages/HomeScreen/HomeScreen";
+import PlanScreen from "./pages/PlanScreen/PlanScreen";
 
 function App() {
-
   return (
     <Router>
       <Routes>
-        <Route exact path="/" element={<HomeScreen />} />
+        <Route path="/" element={<HomeScreen />} />
         <Route path="/plan" element={<PlanScreen />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
