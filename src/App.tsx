73937f62d373b18c6c93dd69f0fc0e92fbd7d1aa
Vuong Ho <hochivuong2002@gmail.com>
2024-06-16 15:34:29 -0400

    Add Privacy Policy and Terms Of Service (#1)
    * Add Privacy Policy
    
    * Add terms of service
    
    * Change font
    
    * Add legal click-accept
    
    * Remove unneeded import
    
    * Change firebase public folder
    
    * Add redirects
    
    * Fix firebasejson
    
    * Change email

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import HomeScreen from './pages/HomeScreen/HomeScreen'
 import PlanScreen from './pages/PlanScreen/PlanScreen'
+import PrivacyPolicy from './pages/PrivacyPolicy/PrivacyPolicy'
+import TermsOfService from './pages/TermsOfService/TermsOfService'
+import Footer from './components/Footer/Footer'
 
 function App() {
   return (
@@ -9,7 +12,10 @@ function App() {
       <Routes>
         <Route path="/" element={<HomeScreen />} />
         <Route path="/plan" element={<PlanScreen />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        <Route path="/terms-and-conditions" element={<TermsOfService />} />
       </Routes>
+      <Footer />
     </Router>
   );
 }
