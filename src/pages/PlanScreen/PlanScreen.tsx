42aaa1b9f45494877831ed6808d50daf07f7ab3c
Hoang Le <hoangledo2092004@gmail.com>
2024-06-20 15:47:50 +0700

    Hoang Le: update PlanScreen - Day 1 travel

--- src/pages/PlanScreen/PlanScreen.tsx
+++ src/pages/PlanScreen/PlanScreen.tsx
@@ -1,12 +1,52 @@
-import { Typography, Box, Button, Divider } from "@mui/material";
-import FmdGoodIcon from '@mui/icons-material/FmdGood';
-// import { useEffect, useState } from "react";
+import { Typography, Box, Button, Divider, Stack, Container, Stepper, Step, StepLabel, StepContent, Paper } from "@mui/material"
+import FmdGoodIcon from '@mui/icons-material/FmdGood'
+import { useState } from "react"
+import ReactPlayer from 'react-player'
+
 // import { getActivities } from "../../api/activitiesApi";
 // import Footer from "../../components/Footer/Footer";
 // import React from "react";
 
+const steps = [
+  {
+    label: 'Visit Hanoi’s Famous Train Street',
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    videoUrl: 'https://www.youtube.com/watch?v=LXb3EKWsInQ'
+  },
+  {
+    label: 'Visit Uncle Ho’s Mausoleum',
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    videoUrl: 'https://www.youtube.com/watch?v=XggV42Kof0k'
+  },
+  {
+    label: 'End of Day',
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+    Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.`,
+    videoUrl: 'https://www.youtube.com/watch?v=WRzvydkwSgg'
+  },
+];
+
 const PlanScreen = () => {
   // const [data, setData] = useState(null);
+
+  const [activeStep, setActiveStep] = useState(0);
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleReset = () => {
+    setActiveStep(0);
+  };
 
   // const handleFetch = () => {  
   //   const fetchData = async () => {
@@ -100,7 +140,68 @@ const PlanScreen = () => {
             </Box>
           </Box>
 
-          <Divider />
+          {/* Divider */}
+          <Divider sx={{ width: '100%', marginTop: 2 }} />
+
+          {/* Travel Content */}
+          <Stack spacing={2} sx={{ textAlign: 'left', marginTop: 2, width: '100%'}}>
+            <Container>
+              {/* Day */}
+              <Typography variant="h4">Day 1: Monday, August 31st</Typography>
+              {/* Travel Detail Plan */}
+              <Box sx={{ display: 'flex' }}>
+                <Stepper activeStep={activeStep} orientation="vertical">
+                  {steps.map((step, index) => (
+                    <Step key={step.label}>
+                      <StepLabel
+                        optional={
+                          index === steps.length - 1 ? (
+                            <Typography variant="caption" sx={{ color: 'black' }}>Last activity</Typography>
+                          ) : null
+                        }
+                      >
+                        <Typography variant="h5">{step.label}</Typography>
+                      </StepLabel>
+                      <StepContent>
+                        <Typography>{step.description}</Typography>
+                        <Box sx={{ mb: 2 }}>
+                          <Button
+                            variant="contained"
+                            onClick={handleNext}
+                            sx={{ mt: 1, mr: 1, backgroundColor: 'black', color: 'white' }}
+                          >
+                            {index === steps.length - 1 ? 'Finish' : 'Continue'}
+                          </Button>
+                          <Button
+                            disabled={index === 0}
+                            onClick={handleBack}
+                            sx={{ mt: 1, mr: 1 }}
+                          >
+                            Back
+                          </Button>
+                        </Box>
+                      </StepContent>
+                    </Step>
+                  ))}
+                </Stepper>
+                <Box sx={{ ml: 3, width: '50%' }}>
+                  {activeStep < steps.length && (
+                    <ReactPlayer url={steps[activeStep].videoUrl} controls />
+                  )}
+                </Box>
+                {activeStep === steps.length && (
+                    <Paper square elevation={0} sx={{ p: 3 }}>
+                      <Typography>All steps completed - you&apos;re finished</Typography>
+                      <Button onClick={handleReset} sx={{ mt: 1, mr: 1 }}>
+                        Reset
+                      </Button>
+                    </Paper>
+                  )}
+              </Box>
+              
+            </Container>
+            
+          </Stack>
           
         </Box>
       </>
