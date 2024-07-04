225ea632b82d057a005a6d329442291844e800c5
Vuong Ho <hochivuong2002@gmail.com>
2024-07-04 13:21:50 -0400

    Fix up themes

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
-import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import { darkTheme } from './utils/themes.ts';
 
-const darkTheme = createTheme({
-  palette: {
-    mode: "dark",
-  },
-});
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
