1b2b8b7fe87c08cceeb755a5fe52c11af2a6cdc5
phamlam24 <ptlam61004@gmail.com>
2024-07-04 11:19:56 +0700

    form screen skeleton

--- src/pages/GetStartedScreen/GetStartedScreen.tsx
+++ src/pages/GetStartedScreen/GetStartedScreen.tsx
@@ -3,25 +3,24 @@ import './GetStartedScreen.css'
 import Catchphrase from "../HomeScreen/Catchphrase";
 import { Button, Typography } from "@mui/material";
 // import HorizontalStepper from "../../components/Stepper/HorizontalStepper";
-import CustomHorizontalStepper from "../../components/Stepper/CustomHorizontalStepper";
+// import CustomHorizontalStepper from "../../components/Stepper/CustomHorizontalStepper";
+// import FormPartOne from "../FormScreen/FormPartOne";
 
 const GetStartedScreen: React.FC = () => {
     return (
       <>
         <div className="bg">
-            <img src="./src/assets/background.jpeg" alt=""/>
-            <div className="bg-dark-cover"></div>     
+          <img src="./src/assets/background.jpeg" alt="" />
+          {/* <div className="bg-dark-cover"></div> */}
         </div>
         <div className="main-flex">
           <Catchphrase />
-          <Typography variant="body1" sx={{color: "white"}}>
-            SwipeAndFly is your travel helper with your interests and favorites in mind.
+          <Typography variant="body1" sx={{ color: "white" }}>
+            SwipeAndFly is your travel helper with your interests and favorites
+            in mind.
           </Typography>
           <Button>Get started</Button>
-          <CustomHorizontalStepper 
-          steps={["Step 1", "Step 2", "Step 3"]}
-          nodes={[<>1</>, <>2</>, <>3</>, <>end</>]}
-          />
+          
         </div>
       </>
     );
--- src/pages/HomeScreen/Catchphrase.tsx
+++ src/pages/HomeScreen/Catchphrase.tsx
@@ -3,77 +3,79 @@ import { Box, Typography } from "@mui/material";
 
 const Catchphrase: React.FC = () => {
     return (
-        <>
-            <Box
-              sx={{
-                display: "block",
-                position: "relative",
-                height: "100px",
-                // width: "100%",
-                textAlign: "center",
-                // marginTop: "200px",
-              }}
-            >
-              <Typography
-                variant="h2"
-                sx={{
-                  fontWeight: "bold",
-                  position: "absolute",
-                  top: "3px",
-                  left: "3px",
-                  color: "#FE285880",
-                  fontSize: {
-                    xs: "3rem",
-                    sm: "3rem",
-                    md: "2.9em",
-                    lg: "3.5rem",
-                    xl: "4rem",
-                  },
-                }}
-              >
-                Discover your perfect getaway.
-              </Typography>
-              <Typography
-                variant="h2"
-                sx={{
-                  fontWeight: "bold",
-                  position: "absolute",
-                  top: 0,
-                  left: 0,
-                  color: "#2AF0EA80",
-                  fontSize: {
-                    xs: "3rem",
-                    sm: "3rem",
-                    md: "2.9rem",
-                    lg: "3.5rem",
-                    xl: "4rem",
-                  },
-                }}
-              >
-                Discover your perfect getaway.
-              </Typography>
-              <Typography
-                variant="h2"
-                sx={{
-                  fontWeight: "bold",
-                  position: "absolute",
-                  top: "1.5px",
-                  left: "1.5px",
-                  color: "white",
-                  fontSize: {
-                    xs: "3rem",
-                    sm: "3rem",
-                    md: "2.9rem",
-                    lg: "3.5rem",
-                    xl: "4rem",
-                  },
-                }}
-              >
-                Discover your perfect getaway.
-              </Typography>
-            </Box>
-        </>
-    )
+      <>
+        <Box
+          sx={{
+            display: "block",
+            position: "relative",
+            height: "100px",
+            textAlign: "center",
+          }}
+        >
+          <Typography
+            variant="h2"
+            sx={{
+              fontWeight: "bold",
+              position: "relative",
+              top: 0,
+              left: 0,
+              width: "calc(100% + 6px)",
+
+              color: "#2AF0EA80",
+              fontSize: {
+                xs: "3rem",
+                sm: "3rem",
+                md: "2.9rem",
+                lg: "3.5rem",
+                xl: "4rem",
+              },
+            }}
+          >
+            Discover your perfect getaway.
+          </Typography>
+          <Typography
+            variant="h2"
+            sx={{
+              fontWeight: "bold",
+              position: "absolute",
+              top: "3px",
+              left: "3px",
+              width: "calc(100% + 6px)",
+              color: "#FE285880",
+              fontSize: {
+                xs: "3rem",
+                sm: "3rem",
+                md: "2.9em",
+                lg: "3.5rem",
+                xl: "4rem",
+              },
+            }}
+          >
+            Discover your perfect getaway.
+          </Typography>
+          <Typography
+            variant="h2"
+            sx={{
+              fontWeight: "bold",
+              position: "absolute",
+              top: "1.5px",
+              left: "1.5px",
+              width: "calc(100% + 6px)",
+              color: "white",
+              fontSize: {
+                xs: "3rem",
+                sm: "3rem",
+                md: "2.9rem",
+                lg: "3.5rem",
+                xl: "4rem",
+              },
+            }}
+          >
+            Discover your perfect getaway.
+          </Typography>
+        </Box>
+      </>
+    );
 }
 
 export default Catchphrase;
