4da81b8f6d98ee7666b6ecb8cb39cbd9cedf2b4c
Vuong Ho <hochivuong2002@gmail.com>
2024-07-25 01:30:51 -0400

    Remove unnecessary stuff

--- src/components/VideoSelector/VideoDisplay/VideoDisplay.tsx
+++ src/components/VideoSelector/VideoDisplay/VideoDisplay.tsx
@@ -61,9 +61,6 @@ export default function VideoDisplay({
             checked={videos.has(video.id)}
             onChange={() => {
               handleChangeVid(video);
-              if (selectVidSideEffect) {
-                selectVidSideEffect(video);
-              }
             }}
             color="primary"
           />
